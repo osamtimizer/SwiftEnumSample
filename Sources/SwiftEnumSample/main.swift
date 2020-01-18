3b04858ee2a471ab9aab1b7862fd5ab1d1266e09
osamtimizer <eaten.jb@gmail.com>
2020-01-18 18:23:47 +0900

    add nested enum

--- Sources/SwiftEnumSample/main.swift
+++ Sources/SwiftEnumSample/main.swift
@@ -6,6 +6,15 @@ enum Hoge {
   case foo(val: String)
 }
 
+enum NestedHoge {
+  case simple
+  case nested(nested: Hoge)
+}
+
 let bar = Hoge.foo(val: "baz")
 
 print(bar)
+
+let nested = NestedHoge.nested(nested: bar)
+
+print(nested)
